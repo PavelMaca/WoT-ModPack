fd2242f139740af467c07ca12400bb010f5d912d
Pavel Máca <maca.pavel@gmail.com>
2015-05-02 15:33:30 +0200

    vesion 1.0 for 0.9.7

--- build/make-archive.bat
+++ build/make-archive.bat
@@ -5,7 +5,7 @@ CD ../
 
 REM set variables
 SET game_varsion=0.9.7
-SET mod_version=0.1
+SET mod_version=1.0
 SET output_file="%game_varsion%.Assassik.modpack.v%mod_version%.rar"
 SET input_files=%game_varsion% configs mods TeamHPPools.xml
 
