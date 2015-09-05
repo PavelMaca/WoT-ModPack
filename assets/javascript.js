675f04aa3e9c59a127264ac408fc2f94d5e35d78
Pavel Máca <maca.pavel@gmail.com>
2015-09-05 14:00:59 +0200

    release info loaded by ajax

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('#sidebar').affix({
         offset: {
             top: function () {
@@ -6,4 +6,40 @@ $(document).ready(function(){
             }
         }
     })
+
+
+    $.getJSON(api_url + '/repos/' + repository_nwo + '/releases', function (result) {
+        var lastRelease;
+        for (var i in result) {
+            if (result[i].draft == false && result[i].assets.length > 0) {
+                lastRelease = result[i];
+                break;
+            }
+        }
+
+        $('#rls_name').text(lastRelease.name);
+        //console.log(lastRelease.name);
+
+        //console.log(lastRelease.published_at);
+        var date = new Date(lastRelease.published_at);
+        var dateString =
+            date.getUTCDate() + "." +
+            (date.getUTCMonth() + 1) + ". " +
+            ("0" + date.getUTCHours()).slice(-2) + ":" +
+            ("0" + date.getUTCMinutes()).slice(-2);
+        $('#rls_date').text(dateString);
+
+        //console.log(lastRelease.assets[0].browser_download_url);
+        $('#rls_url').attr('href', lastRelease.assets[0].browser_download_url);
+
+        //console.log(lastRelease.assets[0].download_count);
+        $('#rls_counter').text(lastRelease.assets[0].download_count);
+
+        //console.log(lastRelease.body);
+        var converter = new showdown.Converter();
+        var changelog = converter.makeHtml(lastRelease.body);
+        //console.log(changelog);
+        $('#rls_changelog').html(changelog);
+    });
+
 });
