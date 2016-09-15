62d6b370e182b8ad6c28fc8ff7e63c4feb64921f
Pavel Máca <maca.pavel@gmail.com>
2016-09-15 22:36:02 +0200

    build 0.9.15.2_v1

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
 REM set variables
-SET game_version=0.9.15.1.1
-SET mod_version=3.0
+SET game_version=0.9.15.2
+SET mod_version=1.0
 SET output_file="%game_version%.Assassik.modpack.v%mod_version%.rar"
 SET input_files=%game_version% configs mods modpack_version.json
 
